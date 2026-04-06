66bb6a9cff7a97ea2511bb51316f8e99d058a048
Arpita Patra <arpitapatra595@gmail.com>
2026-04-06 10:59:18 +0530

    api data fs

--- EJS_FIRSTPROJECT/index.js
+++ EJS_FIRSTPROJECT/index.js
@@ -1,27 +1,100 @@
 const express=require('express');
 const app=express();
-app.set('view engine','ejs');
+const fs=require('fs');
+// app.set('view engine','ejs');
+const users=require('./users_400.json')
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 // app.get('',(req,res)=>{
 //     res.render('home',{name:'don'});
 // })
 // app.listen(3000);
-app.use((req,res,next)=>{
-    console.log("hello middleware 1....");
-    next();
-});
-app.use((req,res,next)=>{
-    console.log("hello middleware 2.....");
-    next();
-});
+
+// app.use((req,res,next)=>{
+//     console.log("hello middleware 1....");
+//     next();
+// });
+// app.use((req,res,next)=>{
+//     console.log("hello middleware 2.....");
+//     next();
+// });
+// app.get('/',(req,res)=>{
+//     let student={
+//         name:'arpita',
+//         age:19,
+//         email:'arpita.patra_cs24@gla.ac.in',
+//         hobbies:['singing','dance','drawing']
+//     };
+//     res.render('home',{ stu: student});
+// });
+
 app.get('/',(req,res)=>{
-    let student={
-        name:'arpita',
-        age:19,
-        email:'arpita.patra_cs24@gla.ac.in',
-        hobbies:['singing','dance','drawing']
-    };
-    res.render('home',{ stu: student});
+    res.send("welcome to our website");
+})
+
+app.get('/api/users',(req,res)=>{
+    return res.json(users);
+})
+app.get('/user',(req,res)=>{
+    const html=`
+    <ul>
+    ${users.map(user=>`<li>${user.name}</li>`).join(' ')}
+    </ul>
+    `
+    res.send(html);
+})
+
+app.get('/api/users/:id',(req,res)=>{
+    const id=Number(req.params.id);
+    console.log(id);
+    const user=users.find(users=>
+        users.id===id
+    );
+    return res.json(user);
+})
+
+// app.post('/users',(req,res)=>{
+//     //we will do this with mongodb
+//     const newData={
+//         "id": 401,
+//         "name": "User401",
+//         "email": "user1@example.com",
+//         "phone": "+917207805264",
+//         "address": "Street 1, City 1, India",
+//         "age": 58
+//     }
+//     fs.appendFile('users_400.json',JSON.stringify(newData),(err)=>{
+//         console.log(err);
+//     });
+//     res.send("file updated");
+// })
+
+// app.post('/api/users', (req, res) => {
+//     const data = req.body; 
+//     console.log(data); 
+//     return res.json({ msg: "success", data: data });
+// });
+
+app.post('/api/users', (req, res) => {
+    const data = req.body;
+
+    fs.writeFile(
+        "users_400.json",
+        JSON.stringify(data),
+        (err) => {
+            if (err) return res.json({ msg: "error" });
+
+            return res.json({ msg: "success",data:data });
+        }
+    );
 });
+app.patch('/api/users/:id',(req,res)=>{
+    //we will do with mongodb 
+})
+
+app.delete('/api/users/:id',(req,res)=>{
+
+})
 app.listen(3500,()=>{
     console.log("server running on port 3500");
 });
